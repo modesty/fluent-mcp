3306c4149da2ce63e952c48c0b62a525eb3202b2
Modesty Zhang <modestyz@hotmail.com>
2025-10-21 15:32:35 -0700

    maint: dependency cleanup (#8)
    * maint: bump dependencies versions
    
    * refactor: remove unused Rollup plugins and ESLint configuration
    
    - Removed @rollup/plugin-eslint from devDependencies and commented out its usage in rollup.config.mjs due to lack of support for ESLint v9+ flat config.
    - Removed rollup-plugin-node-builtins and rollup-plugin-sourcemaps from devDependencies and rollup.config.mjs as they are no longer needed.
    
    * doc: update doc and readme on MCP inspector

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,10 +1,7 @@
 // rollup.config.mjs - Using .mjs extension to force ES modules mode
 import json from '@rollup/plugin-json';
-// import eslint from '@rollup/plugin-eslint';
 import nodeResolve from '@rollup/plugin-node-resolve';
-import builtins from 'rollup-plugin-node-builtins';
 import terser from '@rollup/plugin-terser';
-import sourcemaps from 'rollup-plugin-sourcemaps';
 import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 
@@ -40,28 +37,16 @@ export default [
     plugins: [
       typescript({ tsconfig: './tsconfig.json' }),
       json(),
-      // comment out the eslint plugin till @rollup/plugin-eslint starts to support ESLint v9+ flat config 
-      // eslint({
-      //   throwOnError: true, 
-      //   useEslintrc: false,
-      //   overrideConfig: {
-      //     parserOptions: {
-      //       project: './tsconfig.json',
-      //     },
-      //   },
-      // }),
       nodeResolve({
         preferBuiltins: true,
-        exportConditions: ['node'], // Add node export condition
+        exportConditions: ['node'],
       }),
-      commonjs(), // Add commonjs plugin to handle CommonJS modules
-      builtins(),
+      commonjs(),
       terser({
         format: {
           comments: false
         },
       }),
-      sourcemaps(),
     ]
   }
 ];
